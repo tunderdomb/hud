5e1b960629c102b53002d551d2692479e05d5540
tunderdomb <tunderdomb@gmail.com>
2014-02-27 12:36:13 +0100

    selector bug

--- Scroller/Scroller.js
+++ Scroller/Scroller.js
@@ -21,8 +21,8 @@
     }
     this.root = root
 
-    this.targets = targets = root.querySelectorAll('[role=~"scroller:target"]')
-    this.links = [].slice.call(root.querySelectorAll('[role=~"scroller:link"]')).forEach(function( link ){
+    this.targets = targets = root.querySelectorAll('[role~="scroller:target"]')
+    this.links = [].slice.call(root.querySelectorAll('[role~="scroller:link"]')).forEach(function( link ){
       link.addEventListener("click", function ( e ){
         if( S.busy ) return
         S.busy = true
