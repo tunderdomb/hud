0b37078725cdca8b2c3751314c8705f638a9b16a
tunderdomb <tunderdomb@gmail.com>
2014-04-08 14:43:23 +0200

    get computed styles

--- src/plugin/dom.js
+++ src/plugin/dom.js
@@ -1,39 +1,39 @@
 hud.Role.extend({
-  appendChild: function( element ){
+  appendChild: function ( element ){
     this.element.appendChild(element)
     return this
   },
-  appendTo: function( element ){
+  appendTo: function ( element ){
     element.parentNode.appendChild(this.element)
     return this
   },
-  prependChild: function( element ){
-    if( this.element.firstChild ) {
+  prependChild: function ( element ){
+    if ( this.element.firstChild ) {
       this.element.insertBefore(element, this.element.firstChild)
     }
-    else{
+    else {
       this.element.appendChild(element)
     }
     return this
   },
-  prependTo: function( element ){
-    if( element.firstChild ) {
+  prependTo: function ( element ){
+    if ( element.firstChild ) {
       element.insertBefore(this.element, element.firstChild)
     }
-    else{
+    else {
       element.appendChild(this.element)
     }
     return this
   },
-  replaceChild: function( newElement, refElement ){
+  replaceChild: function ( newElement, refElement ){
     this.element.replaceChild(newElement, refElement)
     return this
   },
-  replaceTo: function( child ){
+  replaceTo: function ( child ){
     child.parentNode.replaceChild(this.element, child)
     return this
   },
-  insertAfter: function( newElement, refElement ){
+  insertAfter: function ( newElement, refElement ){
     if ( refElement.nextSibling ) {
       this.element.insertBefore(newElement, refElement.nextSibling)
     }
@@ -42,7 +42,7 @@ hud.Role.extend({
     }
     return this
   },
-  insertElementAfter: function( element ){
+  insertElementAfter: function ( element ){
     if ( element.nextSibling ) {
       element.parentNode.insertBefore(this.element, element.nextSibling)
     }
@@ -51,23 +51,23 @@ hud.Role.extend({
     }
     return this
   },
-  insertBefore: function( newElement, refElement ){
+  insertBefore: function ( newElement, refElement ){
     this.element.insertBefore(newElement, refElement)
     return this
   },
-  insertElementBefore: function( element ){
+  insertElementBefore: function ( element ){
     element.parentNode.insertBefore(this.element, element)
     return this
   },
-  removeElement: function(  ){
+  removeElement: function (){
     this.element.parentNode.removeChild(this.element)
     return this
   },
-  removeChild: function( childNode ){
+  removeChild: function ( childNode ){
     this.element.removeChild(childNode)
     return this
   },
-  swapChildren: function( child1, child2 ){
+  swapChildren: function ( child1, child2 ){
     var nextSibling = child1.nextSibling
     child2.parentNode.replaceChild(child1, child2)
     if ( nextSibling ) {
@@ -77,7 +77,7 @@ hud.Role.extend({
       nextSibling.parentNode.appendChild(child2)
     }
   },
-  swapElement: function( anotherElement ){
+  swapElement: function ( anotherElement ){
     var nextSibling = this.element.nextSibling
     anotherElement.parentNode.replaceChild(this.element, anotherElement)
     if ( nextSibling ) {
@@ -87,19 +87,19 @@ hud.Role.extend({
       nextSibling.parentNode.appendChild(anotherElement)
     }
   },
-  setAttribute: function( name, value ){
+  setAttribute: function ( name, value ){
     this.element.setAttribute(name, value)
     return this
   },
-  removeAttribute: function( name ){
+  removeAttribute: function ( name ){
     this.element.removeAttribute(name)
     return this
   },
-  getAttribute: function( name ){
+  getAttribute: function ( name ){
     this.element.getAttribute(name)
     return this
   },
-  textContent: function( string ){
+  textContent: function ( string ){
     if ( string === undefined ) {
       return this.element.textContent
     }
@@ -107,7 +107,7 @@ hud.Role.extend({
       return this.element.textContent = string
     }
   },
-  value: function( value ){
+  value: function ( value ){
     if ( value === undefined ) {
       return this.element.value
     }
@@ -115,7 +115,7 @@ hud.Role.extend({
       return this.element.value = value
     }
   },
-  innerHTML: function( html ){
+  innerHTML: function ( html ){
     if ( html === undefined ) {
       return this.element.innerHTML
     }
@@ -123,18 +123,33 @@ hud.Role.extend({
       return this.element.innerHTML = html
     }
   },
-  style: function( prop, value ){
-    if ( value === undefined ) {
-      if ( typeof prop === "string" ) {
-        return this.element.style[prop]
+  style: (function(  ){
+    function getStyle( el, prop ){
+      var value = ""
+      if ( window.getComputedStyle ) {
+        value = getComputedStyle(el).getPropertyValue(prop)
       }
-      else for( var name in prop ){
-        this.element.style[name] = prop[name]
+      else if ( el.currentStyle ) {
+        try {
+          value = el.currentStyle[prop]
+        }
+        catch ( e ) {}
       }
+      return value;
     }
-    else {
-      this.element.style[prop] = value
+    return function ( prop, value ){
+      if ( value === undefined ) {
+        if ( typeof prop === "string" ) {
+          return getStyle(this.element, prop)
+        }
+        else for ( var name in prop ) {
+          this.element.style[name] = getStyle(this.element, name)
+        }
+      }
+      else {
+        this.element.style[prop] = value
+      }
+      return this
     }
-    return this
-  }
+  }(  ))
 })
